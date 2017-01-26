354737e18286855486bdfae0e436d849b2ba7b25
irvinemd55 <irvinedowning@gmail.com>
2017-01-25 16:05:47 -0800

    photo model,route,test added

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ app.use(morgan('dev'));
 //app routes
 app.use(require('./route/auth-router.js'));
 app.use(require('./route/gallery-router.js'));
+app.use(require('./route/photo-router.js'));
 //app error middleware
 app.use(function(err, req, res, next){
   console.log(err.message);
